65571f231cea52a81112257fea35f63186b76ecc
not gonna tell ya <eedduxx@gmail.com>
2023-03-09 01:05:36 -0500

    mail terminado

--- public/src/components/mainApp/mainApp.js
+++ public/src/components/mainApp/mainApp.js
@@ -6,8 +6,19 @@ export const renderMainApp = ({
 }) => {
     const figure = document.createElement("figure");
     figure.classList.add("mainApp");
+    const leftDiv = document.createElement("div")
+    leftDiv.classList.add("leftDiv");
+    const midDiv = document.createElement("div")
+    midDiv.classList.add("midDiv");
+    const rightDiv = document.createElement("div")
+    rightDiv.classList.add("rightDiv");
 
-
+    const checkbox = document.createElement("img");
+    checkbox.classList.add("mailImg");
+    const star = document.createElement("img")
+    star.classList.add("mailImg");
+    const mark = document.createElement("img");
+    mark.classList.add("mailImg");
     const send = document.createElement("p");
     send.classList.add("senderMainApp");
     const sub = document.createElement("p");
@@ -17,15 +28,24 @@ export const renderMainApp = ({
     const dat = document.createElement("p");
     dat.classList.add("dateMainApp");
 
+    checkbox.src = "/public/src/img/check_box.png"
+    star.src = "/public/src/img/star_line.png"
+    mark.src = "/public/src/img/send_line.png"
     send.textContent = sender;
     sub.textContent = subject;
     bod.textContent = body;
     dat.textContent = date;
 
-    figure.appendChild(send);
-    figure.appendChild(sub);
-    figure.appendChild(bod);
-    figure.appendChild(dat);
+    leftDiv.appendChild(checkbox);
+    leftDiv.appendChild(star);
+    leftDiv.appendChild(mark);
+    leftDiv.appendChild(send);
+    midDiv.appendChild(sub);
+    midDiv.appendChild(bod);
+    rightDiv.appendChild(dat);
+    figure.appendChild(leftDiv);
+    figure.appendChild(midDiv);
+    figure.appendChild(rightDiv);
 
     return figure;
 };
--- public/src/components/mainApp/mainAppInfo.js
+++ public/src/components/mainApp/mainAppInfo.js
@@ -3,12 +3,133 @@ export default [
         sender: "Google",
         subject: "Alerta de seguridad",
         body: "Nuevo Inicio de sesion en Windows",
-        date: "17:52"
+        date: "Mar 4"
     },
     {
         sender: "AnneSolarte",
         subject: "AnneSolarte invited you to AnneSolarte/LAB-2",
         body: "GitHub Home",
-        date: "17:33"
+        date: "Mar 4"
+    },
+    {
+        sender: "WeTransfer",
+        subject: "Parcial 1 - Dca",
+        body: "Thanks for using WeTransfer!, it means a lot!",
+        date: "Mar 3"
+    },
+    {
+        sender: "Rectoria - Universidad ICESI",
+        subject: "Comunidado de Rectoria",
+        body: "Comunicado del rector Estimadas y estimados estudiantes",
+        date: "Mar 3"
+    },{
+        sender: "Google",
+        subject: "Alerta de seguridad",
+        body: "Nuevo Inicio de sesion en Windows",
+        date: "Mar 4"
+    },
+    {
+        sender: "AnneSolarte",
+        subject: "AnneSolarte invited you to AnneSolarte/LAB-2",
+        body: "GitHub Home",
+        date: "Mar 4"
+    },
+    {
+        sender: "WeTransfer",
+        subject: "Parcial 1 - Dca",
+        body: "Thanks for using WeTransfer!, it means a lot!",
+        date: "Mar 3"
+    },
+    {
+        sender: "Rectoria - Universidad ICESI",
+        subject: "Comunidado de Rectoria",
+        body: "Comunicado del rector Estimadas y estimados estudiantes",
+        date: "Mar 3"
+    },{
+        sender: "Google",
+        subject: "Alerta de seguridad",
+        body: "Nuevo Inicio de sesion en Windows",
+        date: "Mar 4"
+    },
+    {
+        sender: "AnneSolarte",
+        subject: "AnneSolarte invited you to AnneSolarte/LAB-2",
+        body: "GitHub Home",
+        date: "Mar 4"
+    },
+    {
+        sender: "WeTransfer",
+        subject: "Parcial 1 - Dca",
+        body: "Thanks for using WeTransfer!, it means a lot!",
+        date: "Mar 3"
+    },
+    {
+        sender: "Rectoria - Universidad ICESI",
+        subject: "Comunidado de Rectoria",
+        body: "Comunicado del rector Estimadas y estimados estudiantes",
+        date: "Mar 3"
+    },{
+        sender: "Google",
+        subject: "Alerta de seguridad",
+        body: "Nuevo Inicio de sesion en Windows",
+        date: "Mar 4"
+    },
+    {
+        sender: "AnneSolarte",
+        subject: "AnneSolarte invited you to AnneSolarte/LAB-2",
+        body: "GitHub Home",
+        date: "Mar 4"
+    },
+    {
+        sender: "WeTransfer",
+        subject: "Parcial 1 - Dca",
+        body: "Thanks for using WeTransfer!, it means a lot!",
+        date: "Mar 3"
+    },
+    {
+        sender: "Rectoria - Universidad ICESI",
+        subject: "Comunidado de Rectoria",
+        body: "Comunicado del rector Estimadas y estimados estudiantes",
+        date: "Mar 3"
+    },{
+        sender: "Google",
+        subject: "Alerta de seguridad",
+        body: "Nuevo Inicio de sesion en Windows",
+        date: "Mar 4"
+    },
+    {
+        sender: "AnneSolarte",
+        subject: "AnneSolarte invited you to AnneSolarte/LAB-2",
+        body: "GitHub Home",
+        date: "Mar 4"
+    },
+    {
+        sender: "WeTransfer",
+        subject: "Parcial 1 - Dca",
+        body: "Thanks for using WeTransfer!, it means a lot!",
+        date: "Mar 3"
+    },
+    {
+        sender: "Rectoria - Universidad ICESI",
+        subject: "Comunidado de Rectoria",
+        body: "Comunicado del rector Estimadas y estimados estudiantes",
+        date: "Mar 3"
+    },{
+        sender: "Google",
+        subject: "Alerta de seguridad",
+        body: "Nuevo Inicio de sesion en Windows",
+        date: "Mar 4"
+    },
+    {
+        sender: "AnneSolarte",
+        subject: "AnneSolarte invited you to AnneSolarte/LAB-2",
+        body: "GitHub Home",
+        date: "Mar 4"
+    },
+    {
+        sender: "WeTransfer",
+        subject: "Parcial 1 - Dca",
+        body: "Thanks for using WeTransfer!, it means a lot!",
+        date: "Mar 3"
     }
 ]
